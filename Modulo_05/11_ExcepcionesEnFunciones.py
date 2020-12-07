ce1cbf088e85bdc23ca5cb24bef7b2399cddbf78
Fredyglz <fredyglz.fr@gmail.com>
2020-12-07 04:01:53 -0600

    excepciones, riase y assert

---
+++ Modulo_05/11_ExcepcionesEnFunciones.py
@@ -0,0 +1,18 @@
+﻿def badFun(n):
+    try:
+        return 1 / n
+    except ArithmeticError:
+        print("¡Problema aritmético!")
+    return None
+
+def banFun2(n):
+    return 1 / n;
+
+badFun(0)
+
+try:
+    banFun2(0)
+except ArithmeticError:
+    print('¿Que pasó? ¡Se lanzo una excepción!')
+
+print("FIN.")
