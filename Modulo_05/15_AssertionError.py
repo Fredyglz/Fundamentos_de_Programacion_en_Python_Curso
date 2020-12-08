95d9dd506522bd8779f5f916aa4aabdc2738d8f9
Fredyglz <fredyglz.fr@gmail.com>
2020-12-08 06:34:00 -0600

    Excepciones Útiles

---
+++ Modulo_05/15_AssertionError.py
@@ -0,0 +1,6 @@
+﻿from math import tan, radians
+angle = int(input('Ingresa el angulo entero en grados: '))
+
+# debemos estar seguros de ese angulo != 90 + k * 180
+assert angle % 180 != 90
+print(tan(radians(angle)))
