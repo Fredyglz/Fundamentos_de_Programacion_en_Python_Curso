ece5c8e25f70c6cd9cbcc9e90113d728a2a72d33
Fredyglz <fredyglz.fr@gmail.com>
2020-12-10 03:44:43 -0600

    Naturaleza de las Cadenas

---
+++ Modulo_05/25_OperacionesConCadenas.py
@@ -0,0 +1,53 @@
+﻿# Concatenacion y Replica
+str1 = 'a'
+str2 = 'b'
+
+print(str1 + str2)
+print(str2 + str1)
+print(5 * 'a')
+print('b' * 4)
+
+# Demostrando la función ord ()
+ch1 = 'a' 
+ch2 = ' ' # espacio
+ch3 = 'α'
+ch4 = 'ę'
+
+print(ord(ch1))
+print(ord(ch2))
+print(ord(ch3))
+print(ord(ch4))
+
+# Demostrando la función chr()
+print(chr(97))
+print(chr(945))
+
+# Demonstrando min() 
+print(min("aAbByYzZ"))
+
+t = 'Los Caballeros Que Dicen "¡Ni!"'
+print('[' + min(t) + ']')
+
+t = [0, 1, 2]
+print(min(t))
+
+# Demostrando max() 
+print(max("aAbByYzZ"))
+
+t = 'Los Caballeros Que Dicen "¡Ni!"'
+print('[' + max(t) + ']')
+
+t = [0, 1, 2]
+print(max(t))
+
+# Demonstrando el método index()
+print("aAbByYzZaA".index("b"))
+print("aAbByYzZaA".index("Z"))
+print("aAbByYzZaA".index("A"))
+
+# Demostrando la función list()
+print(list("abcabc"))
+
+# Demostrando el método count()
+print("abcabc".count("b"))
+print('abcabc'.count("d"))
---
+++ Modulo_05/26_CadenasComoSecuencias.py
@@ -0,0 +1,46 @@
+﻿# Indexando cadenas
+exampleString = 'silly walks'
+
+for ix in range(len(exampleString)):
+    print(exampleString[ix], end=' ')
+print()
+for ix in range(len(exampleString)-1, -1, -1):
+    print(exampleString[ix], end=' ')
+print()
+
+# Iterando a través de una cadena
+for ch in exampleString:
+    print(ch, end=' ')
+
+# Rodajas o rebanadas
+alpha = "abdefg"
+
+print(alpha[1:3])
+print(alpha[3:])
+print(alpha[:3])
+print(alpha[3:-2])
+print(alpha[-3:4])
+print(alpha[::2])
+print(alpha[1::2])
+
+# Operadores in y not in
+alpfabeto = "abcdefghijklmnopqrstuvwxyz"
+
+print("f" in alpfabeto)
+print("F" in alpfabeto)
+print("1" in alpfabeto)
+print("ghi" in alpfabeto)
+print("Xyz" in alpfabeto)
+
+# Las cadenas de Python son inmutables
+# Todo este codigo descomentado causara error
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
+
+# Metodo del
+# del alfabeto[0]
+
+# Función append()
+# alfabeto.append("A")
+
+# Función insert()
+# alfabeto.insert(0, "A")
