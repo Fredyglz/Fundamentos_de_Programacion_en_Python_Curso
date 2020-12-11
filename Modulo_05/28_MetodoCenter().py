733808f76cdf96b3c6b3c03695ab058ab94cfa56
Fredyglz <fredyglz.fr@gmail.com>
2020-12-11 06:22:39 -0600

    Metodos de cadenas - 1ra parte

---
+++ Modulo_05/28_MetodoCenter().py
@@ -0,0 +1,8 @@
+﻿# Demostración del método center()
+print('[' + 'alfa'.center(10) + ']')
+print('[' + 'Beta'.center(2) + ']')
+print('[' + 'Beta'.center(4) + ']')
+print('[' + 'Beta'.center(6) + ']')
+
+# Demostración del método center() con dos parametros
+print('[' + 'gamma'.center(20, '*') + ']')
---
+++ Modulo_05/29_MetodoEndswith().py
@@ -0,0 +1,10 @@
+﻿# Demostración del método endswith()
+if "epsilon".endswith("on"):
+    print("si")
+else:
+    print("no")
+t = "zeta"
+print(t.endswith("a"))
+print(t.endswith("A"))
+print(t.endswith("et"))
+print('zeta'.endswith('eta'))
